77b00d0d20de97e4b1741b4b1f7550bfc1355311
Amir Sadykov <amir4ik11@gmail.com>
2025-07-09 21:21:49 +0400

    - Added detekt plugin and setup detektGenerateConfig; - When launched and finished ./gradlew detekt.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.detekt)
     id ("com.google.dagger.hilt.android") version "2.56.2" apply false
 }
